1be6fcc09f890f9f32faf448083556e4d69f04f5
Anshika Kumari <anshikakumari571@gmail.com>
2024-12-14 19:49:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -8,15 +8,55 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    
+    int first(vector<int> &ar,int x){
+        int low=0;
+        int high=ar.size()-1;
+        int first=-1;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(ar[mid] == x){
+                first=mid;
+                high=mid-1;
+            }
+            
+            else if(ar[mid]<x) low=mid+1;
+            
+            else high=mid-1;
+        }
+        return first;
+    }
+    
+    int last(vector<int> &ar,int x){
+        int low=0;
+        int high=ar.size()-1;
+        int last=-1;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(ar[mid] == x){
+                last=mid;
+                low=mid+1;
+            }
+            
+            else if(ar[mid]<x) low=mid+1;
+            
+            else high=mid-1;
+        }
+        return last;
+    }
+    
+    
     int countFreq(vector<int>& arr, int target) {
         // code here
-        int cnt=0;
-        for(int i=0;i<arr.size();i++)
-        {
-            if(arr[i] == target) cnt++;
-        }
         
-        return cnt;
+        int fs=first(arr,target);
+        if (fs == -1) return 0;
+        int ls=last(arr,target);
+        
+        return ls-fs+1;
+        
     }
 };
 
